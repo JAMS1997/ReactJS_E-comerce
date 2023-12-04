ea51802bba5896804e75b00612fd855249c1ff33
Jhon Melendez <jhonmelendez77@gmail.com>
2023-12-04 12:06:07 -0500

    Se inicia el proyecto y se crea la estructura

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p className='bg-red-100'>bayalo bebe </p>
+    </>
+  )
+}
+
+export default App
