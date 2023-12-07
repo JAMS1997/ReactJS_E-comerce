5b4a8080e4010d118ab3c215a641be32dd1a0875
Jhon Melendez <jhonmelendez77@gmail.com>
2023-12-07 11:56:34 -0500

    Mostrar productos en el product detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,13 +3,27 @@ import { createContext, useState  } from 'react';
 const ShoppingCartContext = createContext()
 
 const ShoppingCartProvider = ({children}) => {
+    // Shopping Cart . Increment quantity
     const [ count , setCount ] = useState(0)
-    console.log("Count: " ,count)
+    
+    // Product Detail - Open/Close 
+    const [isProductDetailOpen , setIsProductDetailOpen ] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen( true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    // Product detail - Show Product
+    const [ productToShow , setProductToShow ] = useState({})
 
     return(
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            setIsProductDetailOpen,
+            productToShow,
+            setProductToShow,
         }}>
             {children}
         </ShoppingCartContext.Provider>
